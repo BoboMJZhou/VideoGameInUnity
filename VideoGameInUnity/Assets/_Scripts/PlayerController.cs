c80ca3cf8daec750ba37b790860070c19e15da80
BoboMJZhou <zhou970610@csie.ndhu.edu.tw>
2017-12-15 00:25:31 +0800

    Add Script PlayerController and Teach Material

---
+++ VideoGameInUnity/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("LA");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log("LA UPDATE");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(0, 0, 2000)*Time.deltaTime);
+    }
+}
